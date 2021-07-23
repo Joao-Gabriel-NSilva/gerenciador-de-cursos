ae8c765c892e2ac44fa9c4bea23dcb2481b3a613
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-07-23 14:50:20 -0300

    Aula 6

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,14 +1,17 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -25,6 +28,10 @@ public class Curso {
 
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(this.alunos);
 	}
 
 	public void adiciona(Aula aula) {
@@ -44,6 +51,10 @@ public class Curso {
 	public String toString() {
 		return "[Curso: " + this.getNome() + ", " + "tempo total: " + this.getTempo() + " minutos" + ", aulas: "
 				+ this.aulas + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
 	}
 
 }
