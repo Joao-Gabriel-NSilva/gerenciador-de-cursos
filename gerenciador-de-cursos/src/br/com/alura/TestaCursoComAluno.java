e42811999e84cff6819a19964a2f5c1c7e0baca0
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-07-24 14:22:48 -0300

    Aula 8

--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,7 @@
 package br.com.alura;
 
+import java.util.Iterator;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -9,29 +11,34 @@ public class TestaCursoComAluno {
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
 		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
-		
+
 		Aluno a1 = new Aluno("Rodrigo Turini", 34672);
 		Aluno a2 = new Aluno("Guilherme Silveira", 5627);
 		Aluno a3 = new Aluno("Mauricio Aniche", 17645);
-		
+
 		javaColecoes.matricula(a1);
 		javaColecoes.matricula(a2);
 		javaColecoes.matricula(a3);
-		
+
 		System.out.println("Todos os alunos matricuados: ");
-		javaColecoes.getAlunos().forEach(aluno -> {System.out.println(aluno);});
-		
+		Iterator<Aluno> iterador = javaColecoes.getAlunos().iterator();
+		while (iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+		}
+		// o trecho acima é a forma antiga(antes do java 1.5) de iterar por um set, hoje equivale a:
+		// javaColecoes.getAlunos().forEach(aluno -> {System.out.println(aluno);}); <- introduzida no java 1.8
+
 		System.out.println("O aluno " + a1 + " esta matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(a1));
-		
-		
+
 		Aluno turini = new Aluno("Rodrigo Turini", 34672);
 		System.out.println("E esse Turini?");
 		System.out.println(javaColecoes.estaMatriculado(turini));
-		
+
 		System.out.println("O a1 é equals ao turini?");
 		System.out.println(a1.equals(turini));
-		
+
 	}
 
 }
